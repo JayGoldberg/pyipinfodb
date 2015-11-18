1a202ee81dd4daf303b2b78439fe97a185b12660
Jay Goldberg <jaymgoldberg@gmail.com>
2015-11-18 07:21:16 -0800

    Cleanup, main funciton
    make the script callable with some defaults (no sense having a hashbang if it's not directly executable)

--- pyipinfodb.py
+++ pyipinfodb.py
@@ -1,31 +1,13 @@
 #!/usr/bin/env python
- 
-# Copyright (c) 2010, Westly Ward
-# All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the pyipinfodb nor the
-#       names of its contributors may be used to endorse or promote products
-#       derived from this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY Westly Ward ''AS IS'' AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL Westly Ward BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+__author__ = "Westly Ward"
+__copyright__ = "Copyright 2010"
+__credits__ = ["Westly Ward", "Jay Goldberg"]
+__license__ = "BSD 2.0"
+ 
 import json, urllib, urllib2, socket
+import sys
+
 class IPInfo() :
     def __init__(self, apikey) :
         self.apikey = apikey
@@ -58,3 +40,14 @@ class IPInfo() :
         baseurl = "http://api.ipinfodb.com/v2/ip_query_country.php"
         return self.GetIPInfo(baseurl, ip, timezone)
 
+if __name__ == '__main__':
+    precision = 'country' # 'country' or 'city'
+    timezone = False
+    apikey = sys.argv[1]
+    ip = sys.argv[2]
+    newquery = IPInfo(apikey)
+
+    if precision == 'country':
+        print(newquery.GetCountry(ip, timezone))
+    if precision == 'city':
+        print(newquery.GetCity(ip, timezone))
